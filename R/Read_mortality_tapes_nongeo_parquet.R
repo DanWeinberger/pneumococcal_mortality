2d9f201f0a29338157e3df6736f67aaf9b02c3c3
weinbergerlab <dweinber@gmail.com>
2023-05-04 17:15:54 -0400

    fix 2021 race bridging issue

--- R/Read_mortality_tapes_nongeo_parquet.R
+++ R/Read_mortality_tapes_nongeo_parquet.R
@@ -1,17 +1,3 @@
-#library(feather)
-library(data.table)
-library(readr)
-library(pbapply)
-library(dplyr)
-library(parquetize)
-library(reshape2)
-library(tidyr)
-library(pbapply)
-library(arrow)
-library(duckdb)
-
-library(ggplot2)
-library(cdlTools)
 
 #the geographic resolution missing from the public data
 
@@ -22,9 +8,9 @@ library(cdlTools)
 # 
 # all.ds <- pblapply(file.names1, function(x){
 #   d1 <- read_fwf(file=paste0("R:/NCHS_mortality_public/" ,x),
-#                  fwf_positions(start=c(20,61,63,64,65,69,102,445,70,71, 79,484,146,167,174,181,188,195,202,209,216,223,230,237,244,251,258,265,272,279,286,293,300, 806),
-#                                end=c(  20,62,63,64,66,69,105,446,  70,73, 80,486,149, 171,178,185,192,199,206,213,220,227,234,241,248,255,262,269,276,283,290,297,304, 817),
-#                                col_names = c('res_status','education1989','education2003','education_flag','month','sex','year','race','age_detail_class','age_detail_number','agec','hispanic', paste0('icd', 1:21 ), 'occupation' )),
+#                  fwf_positions(start=c(20,61,63,64,65,69,102,445,448, 484,489,   70,71, 79,484,146,167,174,181,188,195,202,209,216,223,230,237,244,251,258,265,272,279,286,293,300, 806),
+#                                end=c(  20,62,63,64,66,69,105,446,448, 486,490,  70,73, 80,486,149, 171,178,185,192,199,206,213,220,227,234,241,248,255,262,269,276,283,290,297,304, 817),
+#                                col_names = c('res_status','education1989','education2003','education_flag','month','sex','year','race','race_imp2021','hisp_2021','race_recode40','age_detail_class','age_detail_number','agec','hispanic', paste0('icd', 1:21 ), 'occupation' )),
 #                   guess_max=10000)
 #   return(d1)
 # })
@@ -37,7 +23,12 @@ library(cdlTools)
 #   return(x)
 #   })
 # 
-#  df1 <- bind_rows(all.ds) %>%
+#  df1 <- bind_rows(all.ds) 
+#  write_parquet(as.data.frame(df1), "R:/NCHS_mortality_public/parquet2/compiled_data.parquet")
+#  
+ 
+# df1 <- open_dataset("R:/NCHS_mortality_public/parquet2", format = "parquet") %>%
+#   to_duckdb() %>%
 #          mutate( all_icd = paste(icd1,icd2,
 #                                  icd3,icd4,
 #                                  icd5,icd6,
@@ -59,30 +50,45 @@ library(cdlTools)
 #                                                               if_else(race %in% c('04','05','18','28','48' ,'68','78') & hisp_recode != 1,5,
 #                                                                       if_else(race %in% c( '06','07','38','58') & hisp_recode != 1,5,999))
 #                                                       )))),
-#              
-#                  a403 = 1*grepl('A403',all_icd),
-#                  j13 = 1*grepl('J13', all_icd),
-#                  b953 = 1*grepl('B953', all_icd),
-#                  g001 = 1*grepl('G001', all_icd),
-#                  pneumococcal =  1*((a403 + j13 + b953 + g001 )>0)  ,
-#                  covid = grepl('U071', all_icd),
-#                  j154 =1 *grepl('J154', all_icd), #streptococcal pneumonia
-#                  j181 =1 *grepl('J181', all_icd), #unspecified lobar pneumonia
-#                  streptococcal_sepsis = 1* ( grepl('A408',all_icd) + grepl('A409',all_icd) )>0,
-# 
+#                  race_recode_alt= if_else(hisp_recode == 1,3,
+#                                           if_else(race_recode40 %in% c('01') & hisp_recode != 1,1,
+#                                                   if_else(race_recode40 %in% c('02') & hisp_recode != 1 ,2,
+#                                                           if_else(race_recode40 %in% c('03') & hisp_recode != 1,4,
+#                                                                   if_else(race_recode40 %in% c('05','07','04','08','09','10') & hisp_recode != 1,5,                                                                                                                if_else(race_recode40 %in% c('06','11','12','13','14') & hisp_recode != 1,5, 999
+#                                                                                                                                                                                                                                                                            
+#                                                                   )))))),
+#                  race_recode_new=if_else(year<2018, race_recode,race_recode_alt ),
+#                 
 #                  age_detail_number = as.numeric(age_detail_number),
-# 
+#                  
 #                  agey = if_else(age_detail_class==2, age_detail_number/12,
 #                                 if_else(age_detail_class==4, age_detail_number/365,
 #                                         if_else(age_detail_class==5, age_detail_number/365/24,
 #                                                 if_else(age_detail_class==6, age_detail_number/365/24/60, age_detail_number
 #                                                 ))))
-#                            )
-# saveRDS(df1, 'R:/NCHS_mortality_public/compiled_data.rds')
- 
+#                  
+#                  ) %>%
+#   dplyr::select(year, month,age_detail_number, sex, starts_with('education'),all_icd,hisp_recode,race_recode,race_recode_alt,race_recode_new,agey) %>%
+#     collect()
+    
+#write_parquet(as.data.frame(df1), "R:/NCHS_mortality_public/parquet3/compiled_data.parquet")
+
+
+                 
+               
+
+
+#saveRDS(df1, 'R:/NCHS_mortality_public/compiled_data.rds')
+
 #Write as a parquet dataset for more efficient access and data parsing
 #rds_to_parquet('R:/NCHS_mortality_public/compiled_data.rds', "R:/NCHS_mortality_public/parquet", partition = "no")
 
+#check change in race coding after 2018
+pneumo.deaths %>%
+  filter(year>=2018 & year<=2020) %>%
+  group_by(race_recode, race_recode_alt)%>%
+  summarise(n=n())%>%
+  pivot_wider(names_from =race_recode, values_from=n)
 
 ## Cause specific deaths
 
