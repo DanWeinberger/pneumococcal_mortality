64ac9dc302c14560e690ca8261867db35dc4b0d0
weinbergerlab <dweinber@gmail.com>
2024-05-21 23:13:30 -0400

    make plot function more flexible to allow for calls to different subsets of data

--- R/death_plot_fun.R
+++ R/death_plot_fun.R
@@ -1,11 +1,16 @@
 
-death_plot_fun <- function(...){
+death_plot_fun <- function(disease='pneumococcal',...){
+  
+  grouping_vars <- quos(...)
+  disease_sym <- ensym(disease)
+  
   plot.ds1 <- pneumo.deaths.adult %>% 
-    group_by( ... ) %>%
+    filter(!!disease_sym == 1) %>%
+    group_by( !!!grouping_vars ) %>%
     summarize(N_deaths=n()) %>%
     ungroup() %>%
     mutate(year=as.factor(year),month=as.numeric(as.character(month))) %>%
-    tidyr::complete( ..., fill=list(N_deaths=0) ) 
+    tidyr::complete( !!!grouping_vars, fill=list(N_deaths=0) ) 
     
   
   plot.cols <- c(rep('gray85',6),'#66c2a5', '#fc8d62','#8da0cb')
